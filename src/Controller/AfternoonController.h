a8f8989f280a688d664e961233cfff184afcd8d2
meanmonkey662 <coolkoch8535@gmail.com>
2016-01-19 12:42:24 -0700

    Added AfternoonController

---
+++ src/Controller/AfternoonController.h
@@ -0,0 +1,25 @@
+/*
+ * AfternoonController.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: kkoc6943
+ */
+
+#ifndef SRC_CONTROLLER_AFTERNOONCONTROLLER_H_
+#define SRC_CONTROLLER_AFTERNOONCONTROLLER_H_
+
+class AfternoonController
+{
+private:
+	int classCount;
+public:
+	AfternoonController();
+	void setClassCount(int classCount);
+	int getClassCount();
+	void start();
+
+};
+
+
+
+#endif /* SRC_CONTROLLER_AFTERNOONCONTROLLER_H_ */
